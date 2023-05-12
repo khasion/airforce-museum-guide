3127cd6cb06b6080254358efe9f3a89b18da91cb
khasion <gkasionis2@gmail.com>
2023-05-12 20:00:35 +0300

    templates

--- mysite/mouseio/urls.py
+++ mysite/mouseio/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "mouseio"
 urlpatterns = [
+    # ex: /mouseio/
     path("", views.index, name="index"),
+    # ex: /mouseio/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /mouseio/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /mouseio/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]
