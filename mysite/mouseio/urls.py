d5058d3abcecd65c01b37258a4be9b6529d402f8
khasion <gkasionis2@gmail.com>
2023-08-28 10:46:27 +0300

    feixed the Carousel

--- mysite/mouseio/urls.py
+++ mysite/mouseio/urls.py
@@ -4,15 +4,16 @@ from . import views
 
 app_name = "mouseio"
 urlpatterns = [
+    # ex: /export
     path('export', views.export_exhibits_csv, name='export_exhibits_csv'),  
-    # ex: /mouseio
+    # ex: /
     path("", views.index, name="index"),
-    # ex: /mouseio/lang=en
+    # ex: /lang=en
     path("lang=<str:lang>", views.index, name="index"),
-    # ex: /mouseio/about/lang=en
+    # ex: /about/lang=en
     path("about/lang=<str:lang>", views.about, name="about"),
-    # ex: /mouseio/SOUTH/lang=el
+    # ex: /SOUTH/lang=el
     path("<str:location_name>/lang=<str:lang>", views.location, name="location"),
-    # ex: /mouseio/plane=MDD RF-4E Phantom II (69-7487)/lang=en
+    # ex: desc/plane=MDD RF-4E Phantom II (69-7487)/lang=en
     path("desc/plane=<str:plane_name>/lang=<str:lang>", views.description, name="description"),
 ]
